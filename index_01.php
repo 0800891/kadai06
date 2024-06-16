4bd0e9f71a61036af10c336d5d1d5bd7529eb094
Mitsuhashi Kohsaku <mitsuhashi.kousaku@gmail.com>
2024-06-17 00:21:09 +0900

    写真読み込み機能追加

--- index_01.php
+++ index_01.php
@@ -6,21 +6,87 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="write_01.php" method="post">
-    構造設計者名:<input type="text" name="name"><br>
-    建物名称:<input type="text" name="email"><br>
+    <!-- <form action="write_01.php" method="post"> -->
+    構造設計者名:<input type="text" name="name" id='name'><br>
+    建物名称:<input type="text" name="email" id='text_building_name'><br>
         <!-- Note:<textarea name="memo" cols='30' rows='10' id=""></textarea> -->
-        住所:<input type="text" name='address'></textarea><br>
-            <select name="Structural_Design_Code" id="design_code">
+        住所:<input type="text" name='address' id='address'></textarea><br>
+        設計基準： <select name="Structural_Design_Code" id="design_code">
               <option value="">設計基準を選んでください</option>
               <option value="Japanese_Code">Japanese_Code</option>
               <option value="American_Code">American_Code</option>
               <option value="Euro_Code">Euro_Code</option>
             </select>            
-            <!-- <p id="img_text">登録したい画像のファイルを選択してください</p>
-            <input type="file" id="imgUpload"> -->
-        <button type='submit'>send</button>
+            <p id="img_text">登録したい画像のファイルを選択してください</p>
+            <input type="file" id="imgUpload"><br>
+            <!-- <button type='submit'>send</button><br> -->
         <!-- <input type="submit" value='send'> -->
-    </form>
+        <button id="send">送信</button>
+    <!-- </form> -->
 </body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script>
+    
+    // const img =  $("#imgUpload").val();
+
+    
+    
+    // const uploadPhotoButton = document.querySelector('#imgUpload');
+    // uploadPhotoButton.addEventListener("change", function() {        
+    //         let file = uploadPhotoButton.files[0]; //読み込んだファイルを格納
+    //         const reader = new FileReader(); //ファイル読み込みのためのオブジェクトのインスタンス化
+    //         reader.onload = () => { 
+    //         console.dir(file);        //onload 読み込みが終わったときに発火する
+    //         let photo_0 = reader.result;  //読み込んだ画像ファイルを格納
+    //         // localStorage.setItem('dataURLimg', photo); //localstorageに保存
+    //         console.log(photo_0);
+    //             }
+    //         reader.readAsDataURL(file);     //アップロードファイルのDataURL化
+    //     });
+
+    $("#send").on("click",function(){
+        let name =  $("#name").val();
+        let text_building_name = $("#text_building_name").val();
+        let address = $("#address").val();
+        let selectElement = document.getElementById("design_code");;
+        let design_code = selectElement.value;
+
+
+        
+
+        let xhr = new XMLHttpRequest();
+         xhr.open('POST','./write.php',true);
+        xhr.responseType = 'text';//'text','json','arraybuffer','document','blob'
+        let fd = new FormData();
+        fd.append("name",name);
+        console.log(name,'name');
+        fd.append("email",text_building_name);
+
+        console.log(text_building_name,'text_building_name');
+        fd.append("address",address);
+
+        fd.append("Structural_Design_Code",design_code);
+
+        console.log(design_code,'Design_code');
+
+        const reader = new FileReader(); //ファイル読み込みのためのオブジェクトのインスタンス化
+        const uploadPhotoButton = document.querySelector('#imgUpload');
+        let file = uploadPhotoButton.files[0];
+        reader.readAsDataURL(file)
+          reader.onload = function(){ 
+           console.dir(file);        //onload 読み込みが終わったときに発火する
+           
+           let photo = reader.result;  //読み込んだ画像ファイルを格納
+           let img_src = photo;
+           
+           fd.append("img_src",img_src);
+           console.log(img_src);
+
+        xhr.send(fd);}
+        reader.onerror = function() {
+             console.log(reader.error);
+            };
+    })
+   
+    </script>
 </html>
---
+++ write.php
@@ -0,0 +1,26 @@
+<?php
+// error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+
+$name  = $_POST['name'];
+$email = $_POST['email'];
+// $memo  = $_POST['memo'];
+$address  = $_POST['address'];
+if(isset($_POST['Structural_Design_Code'])){
+    $code  = $_POST['Structural_Design_Code'];
+    // echo $code;
+}
+$img_src = $_POST['img_src'];
+
+$c = ',';
+$str = $name . $c . $email . $c . $address . $c . $code . $c . $img_src . "\n"; //aaaa,bbbb,cccc
+echo $str;
+$arry = explode($c,$str);
+var_dump($arry);
+$file = fopen("data/data.csv","a");
+fwrite($file, $str);
+fclose($file);
+
+// header('Location: index_01.php');
+// exit;
+?>
